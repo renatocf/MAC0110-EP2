1a819e326f69e5ed3dc76ffa5be1e2d8633eb38e
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-05-07 09:32:45 -0300

    implement funcao seno e cosseno

--- FuncMat.java
+++ FuncMat.java
@@ -1,5 +1,40 @@
-class FuncMat {
+class FuncMat 
+{
   
+  double epsilon = 10e-8;
   
+  double sen(double x)
+  {
+    double n = 1;
+    double seno = 0;
+    double termo = x;
+    
+    //for (int n=1; (termo*termo) >= (epsilon * epsilon); n=n+2) 
+    while ((termo*termo) >= (epsilon * epsilon))
+    {
+      seno = seno + termo;
+      n = n + 2;
+      termo = -termo*x*x/(n*(n-1));
+    }
+    
+    return seno;
+  }
+  
+    double cos(double x)
+  {
+    double n = 1;
+    double cosseno = 1;
+    double termo = -x*x/2;
+    
+    //for (int n=1; (termo*termo) >= (epsilon * epsilon); n=n+2) 
+    while ((termo*termo) >= (epsilon * epsilon))
+    {
+      cosseno = cosseno + termo;
+      n = n + 2;
+      termo = -termo*x/n;
+    }
+    
+    return cosseno;
+  }
   
 }
