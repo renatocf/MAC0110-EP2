83932be17637543b8d0ed069d1c1886a105bb0b3
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-05-14 09:42:54 -0300

    adding square root

--- FuncMat.java
+++ FuncMat.java
@@ -1,6 +1,5 @@
 class FuncMat 
 {
-  
   double epsilon = 10e-8;
   
   double sen(double x)
@@ -20,7 +19,7 @@ class FuncMat
     return seno;
   }
   
-    double cos(double x)
+  double cos(double x)
   {
     double n = 1;
     double cosseno = 1;
@@ -37,7 +36,7 @@ class FuncMat
     return cosseno;
   }
     
-        double ln(double umMaisX)
+  double ln(double umMaisX)
   {
     double x = umMaisX - 1;
     int n = 1;
@@ -48,13 +47,29 @@ class FuncMat
     while ((termo*termo) >= (epsilon * epsilon))
     {
       ln = ln + termo;
-      n = n + 2;
+      n = n + 1;
       termo = -termo*x/n;
     }
     
     return ln;
   }
         
-  
+  double raiz(double umMaisX)
+  {
+    double x = umMaisX - 1;
+    int k = -1;
+    double raiz = 0;
+    double termo = 1;
+    
+    //for (int n=1; (termo*termo) >= (epsilon * epsilon); n=n+2) 
+    do // while ((termo*termo) >= (epsilon * epsilon))
+    {
+      raiz = raiz + termo;
+      k = k + 1;
+      termo = (-1) * termo * (1+2*k) * (2*k + 2) * (1-2*k) * x / ((4)*(k+1)*(k+1)*(-1-2*k));
+    } while ((termo*termo) >= (epsilon * epsilon));
+    
+    return raiz;
+  }  
   
 }
