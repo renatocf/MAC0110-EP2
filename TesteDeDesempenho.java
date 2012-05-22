0f05ab36b3bf87f46cbf550a648f283583fe5e66
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-05-22 17:26:16 -0300

    Adding TesteDeDesempenho.java (2nd part of EP2)

--- TesteDeDesempenho.java
+++ TesteDeDesempenho.java
@@ -1,4 +1,16 @@
 class TesteDeDesempenho
 {
-  double[] temposFunçõesComDouble;
+  double[][] tempos = new double[100][4];
+  
+  
+  double[][] carregaTempos()
+  {
+    FunçõesMatemáticas Mat = new FunçõesMatemáticas();
+    
+    for(int i=1; i < 1; i++)
+    {
+      Mat.sen(i/)
+    }
+    return tempos;
+  }
 }
